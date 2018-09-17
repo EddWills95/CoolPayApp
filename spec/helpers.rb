fe47a2df2a6a7e9e9072fd88b18c41e09f5c1ac9
EddWills95 <e.williams9@herts.ac.uk>
2018-09-17 15:09:19 +0100

    fix mocks

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -18,7 +18,12 @@ module Helpers
   end
 
   def mock_recipients
-    stub_request(:get, 'https://coolapy.herokuapp.com/api/recipients')
+    stub_request(:get, "https://coolpay.herokuapp.com/api/recipients")
+      .with(
+        headers: {
+          'Authorization'=>'Bearer aff06fec-e041-4994-849e-223f0569e0bc',
+        }
+      )
       .to_return(
         status: 200, body: {
           recipients: [
@@ -32,39 +37,39 @@ module Helpers
   end
 
   def mock_search_recipients
-    stub_request(:get, 'https://coolapy.herokuapp.com/api/recipients?name=joe')
-      .with(
-        headers: {
-          'Accept' => '*/*',
-          'Accept-Encoding' => 'gzip, deflate',
-          'Authorization' => '{:content_type=>"application/json", :authorization=>"Bearer aff06fec-e041-4994-849e-223f0569e0bc"}',
-          'Host' => 'coolapy.herokuapp.com',
-          'User-Agent' => 'rest-client/2.0.2 (darwin18.0.0 x86_64) ruby/2.5.1p57'
+    stub_request(:get, "https://coolpay.herokuapp.com/api/recipients?name=joe").
+    with(
+      headers: {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip, deflate',
+      'Authorization'=>'{:content_type=>"application/json", :authorization=>"Bearer aff06fec-e041-4994-849e-223f0569e0bc"}',
+      'Host'=>'coolpay.herokuapp.com',
+      'User-Agent'=>'rest-client/2.0.2 (darwin18.0.0 x86_64) ruby/2.5.1p57'
+      }).
+    to_return(status: 200, body: {
+      recipients: [
+        {
+          id: '6e7b4cea-5957-11e6-8b77-86f30ca893d3',
+          name: 'Joe Bloggs'
         }
-      )
-      .to_return(status: 200, body: {
-        recipients: [
-          {
-            id: '6e7b4cea-5957-11e6-8b77-86f30ca893d3',
-            name: 'Joe Bloggs'
-          }
-        ]
-      }.to_json, headers: {})
+      ]
+    }.to_json, headers: {})
   end
 
   def mock_add_recipient
-    stub_request(:post, 'https://coolapy.herokuapp.com/api/recipients')
-      .with(
-        headers: {          
-          'Authorization' => 'Bearer aff06fec-e041-4994-849e-223f0569e0bc',
-        }
-      )
-      .to_return(status: 200, body: {
-        recipient: {
-          id: 'e9a0336b-d81d-4009-9ad1-8fa1eb43418c',
-          name: 'Jeffery Archer'
-        }
-      }.to_json, headers: {})
+    stub_request(:post, "https://coolpay.herokuapp.com/api/recipients").
+    with(
+      body: {"recipient"=>{"name"=>"Jeffery Archer"}},
+      headers: {
+      'Authorization'=>'Bearer aff06fec-e041-4994-849e-223f0569e0bc',
+      'Content-Type'=>'application/x-www-form-urlencoded',
+      }).
+    to_return(status: 200, body: {
+      recipient: {
+        id: 'e9a0336b-d81d-4009-9ad1-8fa1eb43418c',
+        name: 'Jeffery Archer'
+      }
+    }.to_json, headers: {})
   end
 
   def mock_create_payment
